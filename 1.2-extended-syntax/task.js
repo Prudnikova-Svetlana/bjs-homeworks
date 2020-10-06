a3e6f4a26209031a181b1b7564fe51c04c04cab2
SvetlanaPrudnikova <lana.prudnikova.69@bk.ru>
2020-10-06 18:02:30 +0700

    Home work 2 1.2-Extended syntax

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,66 +1,68 @@
 "use strict";
+
 function getResult(a,b,c){
-    let x_1;
-    let x_2;
-    let x = [];
-    let discriminate =  b ** 2 - 4 * a * c;
-    let squareRoot = Math.sqrt(discriminate);
+
+  let x_1;
+  let x_2;
+  let x = [];
+  let discriminate =  b ** 2 - 4 * a * c;
+  let squareRoot = Math.sqrt(discriminate);
     
     if (discriminate > 0) {
-        x_1 = (- b + squareRoot) / (2 * a);
-        x.push(x_1);
-        x_2 = (- b - squareRoot) / (2 * a);
-        x.push(x_2);
+      x_1 = (- b + squareRoot) / (2 * a);
+      x.push(x_1);
+      x_2 = (- b - squareRoot) / (2 * a);
+      x.push(x_2);
     }
+
     if (discriminate === 0) {
-        x_1 = (- b + squareRoot) / 2 * a;
-        x.push(x_1);  
+      x_1 = (- b + squareRoot) / 2 * a;
+      x.push(x_1);  
     }
-if (discriminate < 0) {
-    x = [];
+      return x;
     }
-    return x;
-}
     
 function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-let arrMarks = [];
-let averageMark;
-let sumMark = 0;
-arrMarks = marks;
+    
+  let arrMarks = [];
+  let averageMark = 0;
+  let sumMark = 0;
+  arrMarks = marks;
+  
+    for (let i = 0; i < arrMarks.length; i ++) {
+      sumMark = sumMark + arrMarks[i];
+    
+      if (arrMarks.length === 0) {
+        averageMark = 0;
+      }
+      
+      if (arrMarks.length > 5) {
+        arrMarks.splice(5);
+        console.log("Извините, но количество введенных оценок должно быть меньше 5");
+        averageMark = sumMark / arrMarks.length;  
+      } 
 
-for (let i = 0; i < arrMarks.length; i ++) {
-    sumMark = sumMark + arrMarks[i];
-if (arrMarks.length > 5) {
-    arrMarks.splice(5, (arrMarks.length - 1));
-    console.log("Извините, но количество введенных оценок должно быть меньше 5");
-    console.log(arrMarks);
-    averageMark = sumMark / arrMarks.length;  
-} 
-if (arrMarks.length <= 5) {
-   averageMark = sumMark / arrMarks.length;
-}
-   if (arrMarks = []) {
-    averageMark = 0;
-}
-}
-  return averageMark;
+      if (arrMarks.length <= 5) {
+        averageMark = sumMark / arrMarks.length;
+      }
+    }
+        return averageMark;
+  
 }
 
 function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-let today = new Date();
-let year = today.getFullYear();
-let yearOfBirthday = dateOfBirthday.getFullYear();
-let result = year - yearOfBirthday;
-console.log(result);
-console.log(yearOfBirthday);
-console.log(year);
-if (result >= 18) {
-    console.log("Не желаете ли олд-фэшн, " + name + ", ?");
-}
-else {
-    console.log("Сожалею, " + name + " , но я не могу продать вам алкоголь. Зато могу предложить вам замечательный клюквенный компот!");
-}
-    return result;
+    
+  let today = new Date();
+  let year = today.getFullYear();
+  //let yearOfBirthday = dateOfBirthday.getFullYear();
+  //let age = year - yearOfBirthday;
+  let yearOfBirthday = dateOfBirthday;
+  let age = year - yearOfBirthday.getFullYear();
+  let result;
+  if (age >= 18) {
+    result = `Не желаете ли олд-фэшн, ${name}?`;
+  } else {
+    result = `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
+  }
+  return result;
 }
